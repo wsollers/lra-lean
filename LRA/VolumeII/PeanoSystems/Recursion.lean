aef940a4882498d338c59041abe2e01b854f15ca
William E. Sollers III <wsollers@gmail.com>
2026-07-29 01:32:45 -0400

    Split binary iterator recursion module

--- LRA/VolumeII/PeanoSystems/Recursion.lean
+++ LRA/VolumeII/PeanoSystems/Recursion.lean
@@ -1,4 +1,5 @@
 import LRA.VolumeII.PeanoSystems.Recursion.Iterator
+import LRA.VolumeII.PeanoSystems.Recursion.BinaryIterator
 
 /-!
 Aggregate import for Peano-system recursion and iterator machinery.
