4cfa55b37b7426e30d576cc55f6e9eea9f029b7d
BoondockTaints <wsollers@gmail.com>
2026-08-02 00:24:11 -0400

    Basic Set operations
    Basic set operations completed

--- LRA/VolumeI/Set/Operations/BasicSetOperations.lean
+++ LRA/VolumeI/Set/Operations/BasicSetOperations.lean
@@ -13,30 +13,51 @@ theorem UnionMembershipIff {Alpha : LRACarrier}
     (Left Right : LRASet Alpha) (Element : Alpha) :
     Member Element (Union Left Right) ↔
       Member Element Left ∨ Member Element Right := by
-  sorry
+    constructor
+    · intro elementInLUR
+      cases elementInLUR with
+      | inl elementInLeft => exact Or.inl elementInLeft
+      | inr elementInRight => exact Or.inr elementInRight
+    · intro elementInRUL
+      cases elementInRUL with
+      | inl elementInLeft => exact Or.inl elementInLeft
+      | inr elementInRight => exact Or.inr elementInRight
 
 theorem IntersectionMembershipIff {Alpha : LRACarrier}
     (Left Right : LRASet Alpha) (Element : Alpha) :
     Member Element (Intersection Left Right) ↔
       Member Element Left ∧ Member Element Right := by
-  sorry
+  constructor
+  · intro elementInLIR
+    exact ⟨elementInLIR.left, elementInLIR.right⟩
+  · intro elementInRIL
+    exact ⟨elementInRIL.left, elementInRIL.right⟩
 
 theorem ComplementMembershipIff {Alpha : LRACarrier}
     (Comp : LRASet Alpha) (Element : Alpha) :
     Member Element (Complement Comp) ↔
       ¬ Member Element Comp := by
-  sorry
+  constructor
+  · intro elementInComplement
+    exact elementInComplement
+  · intro elementNotInComp
+    exact elementNotInComp
 
 theorem DifferenceMembershipIff {Alpha : LRACarrier}
     (Left Right : LRASet Alpha) (Element : Alpha) :
     Member Element (Difference Left Right) ↔
       Member Element Left ∧ ¬ Member Element Right := by
-  sorry
+  constructor
+  · intro elementInDifference
+    exact ⟨elementInDifference.left, elementInDifference.right⟩
+  · intro elementInLeftNotInRight
+    exact ⟨elementInLeftNotInRight.left, elementInLeftNotInRight.right⟩
 
 theorem SubsetMembershipImplication {Alpha : LRACarrier}
     (Left Right : LRASet Alpha)
     (LeftSubsetRight : Subset Left Right) :
     ∀ Element : Alpha, Member Element Left → Member Element Right := by
-  sorry
+  intro Element elementInLeft
+  exact LeftSubsetRight Element elementInLeft
 
 end LRA.VolumeI.Set.Operations.BasicSetOperations
