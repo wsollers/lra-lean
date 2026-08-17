35389b4a09fd56fe0bfe703d2d5199c10a73576c
BoondockTaints <wsollers@gmail.com>
2026-08-17 13:50:55 -0400

    refactor: keep default relation calculus backend-neutral

--- LRA/Relation/Calculus.lean
+++ LRA/Relation/Calculus.lean
@@ -1,6 +1,4 @@
 import LRA.Relation.Calculus.Classes.Definition
 import LRA.Relation.Calculus.Classes.Theorems
-import LRA.Relation.Calculus.DomainRange.Definition
+import LRA.Relation.Calculus.DomainRange
 import LRA.Relation.Calculus.Restriction.Definition
-import LRA.VolumeI.Relations.DomainRange.Consequences
-import LRA.VolumeI.Relations.Images.All
