428f29a143392f95e93d3befd9787c15b1c33c5d
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:44:22 -0400

    refactor: canonicalize BottomElement definition

---
+++ LRA/Order/Bounds/BottomElement/Definition.lean
@@ -0,0 +1,20 @@
+import LRA.Order.Bounds.LeastElement.Definition
+import LRA.Set.Interface.Operations
+
+namespace LRA.Order
+
+open scoped LRA.Set
+
+universe u v
+
+variable {Element : Type u} {SetObject : Type v}
+variable [Membership Element SetObject]
+variable [LRA.Set.HasUniversal SetObject]
+
+/-- A bottom element is the least element of the full carrier represented by the universal set. -/
+def BottomElement
+    (relation : LRA.Relation.Endorelation Element)
+    (bottom : Element) : Prop :=
+  LeastElement relation (𝒰 : SetObject) bottom
+
+end LRA.Order
