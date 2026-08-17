1a6706fa6b5942ad3ab6d8a2ae426fc3ad050e83
BoondockTaints <wsollers@gmail.com>
2026-08-17 17:42:38 -0400

    refactor: expose canonical Preorder and PartialOrder theorem layers

--- LRA/Order/OrderedSets.lean
+++ LRA/Order/OrderedSets.lean
@@ -1,5 +1,9 @@
 import LRA.Order.OrderedSets.Preorder.Definition
+import LRA.Order.OrderedSets.Preorder.Characterizations
+import LRA.Order.OrderedSets.Preorder.Theorems
 import LRA.Order.OrderedSets.PartialOrder.Definition
+import LRA.Order.OrderedSets.PartialOrder.NonStrict
+import LRA.Order.OrderedSets.PartialOrder.Characterizations
 import LRA.Order.OrderedSets.LinearOrder.Definition
 import LRA.Order.OrderedSets.StrictOrder.Definition
 import LRA.Order.OrderedSets.StrictLinearOrder.Definition
