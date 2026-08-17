02d2495d9d4cc006fac8413564026f3fe6d08a80
BoondockTaints <wsollers@gmail.com>
2026-08-17 17:36:23 -0400

    refactor: expose canonical ordered-set hierarchy

---
+++ LRA/Order/OrderedSets.lean
@@ -0,0 +1,8 @@
+import LRA.Order.OrderedSets.Preorder.Definition
+import LRA.Order.OrderedSets.PartialOrder.Definition
+import LRA.Order.OrderedSets.LinearOrder.Definition
+import LRA.Order.OrderedSets.StrictOrder.Definition
+import LRA.Order.OrderedSets.StrictLinearOrder.Definition
+import LRA.Order.OrderedSets.Poset.Definition
+
+/-! Canonical foundational ordered-set hierarchy. -/
