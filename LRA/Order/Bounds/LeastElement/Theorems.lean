eaff6a120227db663fb2e04c782f55d8efd7a8b4
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:38:34 -0400

    refactor: canonicalize least-element theorem core

---
+++ LRA/Order/Bounds/LeastElement/Theorems.lean
@@ -0,0 +1,20 @@
+import LRA.Order.Bounds.LeastElement.Definition
+import LRA.Relation.Properties.Definition
+
+namespace LRA.Order
+
+universe u v
+
+/-- Antisymmetry makes a least element unique. -/
+theorem LeastElementUnique
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    (relationIsAntisymmetric : LRA.Relation.Antisymmetric relation)
+    {subset : SetObject} {first second : Element}
+    (firstIsLeast : LeastElement relation subset first)
+    (secondIsLeast : LeastElement relation subset second) :
+    first = second := by
+  sorry
+
+end LRA.Order
