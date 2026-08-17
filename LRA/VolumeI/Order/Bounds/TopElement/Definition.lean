b3e983602c61edd3b0d72bb922e932bd8ca1a24d
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:44:39 -0400

    refactor: make legacy TopElement definition a shim

--- LRA/VolumeI/Order/Bounds/TopElement/Definition.lean
+++ LRA/VolumeI/Order/Bounds/TopElement/Definition.lean
@@ -1,23 +1,3 @@
-import LRA.VolumeI.Order.Bounds.GreatestElement.Definition
-import LRA.VolumeI.Set.Interface.Operations
+import LRA.Order.Bounds.TopElement.Definition
 
-namespace LRA.Order
-
-open scoped LRA.Set
-
-universe u v
-
-variable {Element : Type u} {SetObject : Type v}
-variable [Membership Element SetObject]
-variable [LRA.Set.HasUniversal SetObject]
-
-/--
-A top element is the greatest element of the full carrier represented by the
-universal set.
--/
-def TopElement
-    (relation : LRA.Relation.Endorelation Element)
-    (top : Element) : Prop :=
-  GreatestElement relation (𝒰 : SetObject) top
-
-end LRA.Order
+/-! Legacy forwarding import. -/
