1a7ad0b74a2c0b468d40be6ce474a8e357474b32
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:58:11 -0400

    refactor: make legacy chain definition a shim

--- LRA/VolumeI/Order/Directedness/Chain/Definition.lean
+++ LRA/VolumeI/Order/Directedness/Chain/Definition.lean
@@ -1,19 +1,3 @@
-import LRA.VolumeI.Order.Relation.Comparable.Definition
+import LRA.Order.Directedness.Chain.Definition
 
-namespace LRA.Order
-
-universe u v
-
-/-- A chain is a subset whose elements are pairwise comparable. The empty
-subset is a chain under this convention. -/
-def Chain
-    {Element : Type u} {SetObject : Type v}
-    [Membership Element SetObject]
-    (relation : LRA.Relation.Endorelation Element)
-    (subset : SetObject) : Prop :=
-  forall first second,
-    first ∈ subset ->
-      second ∈ subset ->
-        Comparable relation first second
-
-end LRA.Order
+/-! Legacy forwarding import. -/
