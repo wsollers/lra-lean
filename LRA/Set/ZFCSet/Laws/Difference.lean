4c4e744758375d27b0faa7d30cb22b32c6ced2b6
BoondockTaints <wsollers@gmail.com>
2026-08-17 13:55:17 -0400

    refactor: canonicalize ZFCSet difference law imports

--- LRA/Set/ZFCSet/Laws/Difference.lean
+++ LRA/Set/ZFCSet/Laws/Difference.lean
@@ -1,5 +1,5 @@
-import LRA.VolumeI.Set.Enderton.Instances
-import LRA.VolumeI.Set.Interface.Difference
+import LRA.Set.ZFCSet.Instances
+import LRA.Set.Interface.Difference
 
 namespace LRA.Set.Enderton
 
