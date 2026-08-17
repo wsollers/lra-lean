e188922d91156c33039d6c3f609d201996256528
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:44:49 -0400

    refactor: make legacy BottomElement definition a shim

--- LRA/VolumeI/Order/Bounds/BottomElement/Definition.lean
+++ LRA/VolumeI/Order/Bounds/BottomElement/Definition.lean
@@ -1,23 +1,3 @@
-import LRA.VolumeI.Order.Bounds.LeastElement.Definition
-import LRA.VolumeI.Set.Interface.Operations
+import LRA.Order.Bounds.BottomElement.Definition
 
-namespace LRA.Order
-
-open scoped LRA.Set
-
-universe u v
-
-variable {Element : Type u} {SetObject : Type v}
-variable [Membership Element SetObject]
-variable [LRA.Set.HasUniversal SetObject]
-
-/--
-A bottom element is the least element of the full carrier represented by the
-universal set.
--/
-def BottomElement
-    (relation : LRA.Relation.Endorelation Element)
-    (bottom : Element) : Prop :=
-  LeastElement relation (𝒰 : SetObject) bottom
-
-end LRA.Order
+/-! Legacy forwarding import. -/
