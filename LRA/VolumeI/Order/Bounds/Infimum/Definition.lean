25eb02f4b1814643b97001b5765b03f470690851
BoondockTaints <wsollers@gmail.com>
2026-08-17 18:58:55 -0400

    refactor: make legacy Infimum definition a shim

--- LRA/VolumeI/Order/Bounds/Infimum/Definition.lean
+++ LRA/VolumeI/Order/Bounds/Infimum/Definition.lean
@@ -1,30 +1,3 @@
-import LRA.VolumeI.Order.Bounds.LowerBound.Definition
+import LRA.Order.Bounds.Infimum.Definition
 
-namespace LRA.Order
-
-universe u v
-
-variable {Element : Type u} {SetObject : Type v}
-variable [Membership Element SetObject]
-
-/-- An infimum, or greatest lower bound, of a subset.
-
-Logical form:
-
-```lean
-def Infimum
-    (relation : LRA.Relation.Endorelation Element)
-    (subset : SetObject)
-    (infimum : Element) : Prop :=
-  LowerBound relation subset infimum /\
-    forall bound, LowerBound relation subset bound -> relation bound infimum
-```
--/
-def Infimum
-    (relation : LRA.Relation.Endorelation Element)
-    (subset : SetObject)
-    (infimum : Element) : Prop :=
-  LowerBound relation subset infimum /\
-    forall bound, LowerBound relation subset bound -> relation bound infimum
-
-end LRA.Order
+/-! Compatibility shim for the canonical Infimum definition. -/
