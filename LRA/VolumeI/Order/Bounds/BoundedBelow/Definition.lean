3a3624dba5e35c881ef883cf31886f4a6086527f
BoondockTaints <wsollers@gmail.com>
2026-08-17 18:37:07 -0400

    refactor: make legacy BoundedBelow definition a shim

--- LRA/VolumeI/Order/Bounds/BoundedBelow/Definition.lean
+++ LRA/VolumeI/Order/Bounds/BoundedBelow/Definition.lean
@@ -1,16 +1,3 @@
-import LRA.VolumeI.Order.Bounds.LowerBound.Definition
+import LRA.Order.Bounds.BoundedBelow.Definition
 
-namespace LRA.Order
-
-universe u v
-
-variable {Element : Type u} {SetObject : Type v}
-variable [Membership Element SetObject]
-
-/-- A subset is bounded below when it has a lower bound. -/
-def BoundedBelow
-    (relation : LRA.Relation.Endorelation Element)
-    (subset : SetObject) : Prop :=
-  exists bound, LowerBound relation subset bound
-
-end LRA.Order
+/-! Compatibility import for canonical `LRA.Order.BoundedBelow`. -/
