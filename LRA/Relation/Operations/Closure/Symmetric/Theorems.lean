ba4cbd92580696eb0422a08f7ee1a71c7187130d
BoondockTaints <wsollers@gmail.com>
2026-08-17 14:31:29 -0400

    chore: leave new symmetric closure proofs to learner

--- LRA/Relation/Operations/Closure/Symmetric/Theorems.lean
+++ LRA/Relation/Operations/Closure/Symmetric/Theorems.lean
@@ -9,17 +9,13 @@ universe u
 theorem RelationIncludedInSymmetricClosure
     {Alpha : Type u} (relation : Endorelation Alpha) :
     RelationIncluded relation (SymmetricClosure relation) := by
-  intro left right related
-  exact Or.inl related
+  sorry
 
 /-- The symmetric closure is symmetric. -/
 theorem SymmetricClosureIsSymmetric
     {Alpha : Type u} (relation : Endorelation Alpha) :
     Symmetric (SymmetricClosure relation) := by
-  intro left right related
-  rcases related with related | converseRelated
-  · exact Or.inr related
-  · exact Or.inl converseRelated
+  sorry
 
 /-- The symmetric closure is the least symmetric relation containing the original relation. -/
 theorem SymmetricClosureIsLeast
@@ -27,9 +23,6 @@ theorem SymmetricClosureIsLeast
     (relationIncluded : RelationIncluded relation target)
     (targetSymmetric : Symmetric target) :
     RelationIncluded (SymmetricClosure relation) target := by
-  intro left right related
-  rcases related with related | converseRelated
-  · exact relationIncluded left right related
-  · exact targetSymmetric right left (relationIncluded right left converseRelated)
+  sorry
 
 end LRA.Relation
