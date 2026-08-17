77a17e3c759e42479b2bd08638fe64c765bad866
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:58:01 -0400

    refactor: make legacy comparability a shim

--- LRA/VolumeI/Order/Relation/Comparable/Definition.lean
+++ LRA/VolumeI/Order/Relation/Comparable/Definition.lean
@@ -1,28 +1,3 @@
-import LRA.VolumeI.Relations.Basic.Relations
+import LRA.Order.Relation.Comparable.Definition
 
-namespace LRA.Order
-
-universe u
-
-/--
-Two elements are comparable with respect to a displayed relation.
-
-For a non-strict order this is the usual comparability predicate. A strict
-order requires a separate equality alternative; that relationship belongs in
-the concept's future characterization layer.
-
-Logical form:
-
-```lean
-def Comparable {alpha : Type u}
-    (relation : LRA.Relation.Endorelation alpha)
-    (left right : alpha) : Prop :=
-  relation left right \/ relation right left
-```
--/
-def Comparable {alpha : Type u}
-    (relation : LRA.Relation.Endorelation alpha)
-    (left right : alpha) : Prop :=
-  relation left right \/ relation right left
-
-end LRA.Order
+/-! Legacy forwarding import. -/
