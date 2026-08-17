04734b5cc734a63b2d7441958a7284030a8bc042
BoondockTaints <wsollers@gmail.com>
2026-08-17 15:12:50 -0400

    refactor: complete canonical Function calculus definitions

--- LRA/Function/Calculus/Definition.lean
+++ LRA/Function/Calculus/Definition.lean
@@ -1,10 +1,19 @@
 import LRA.Function.Definition
 import LRA.Relation.Calculus.Classes.Definition
+import LRA.Relation.Calculus.Restriction.Definition
 
 namespace LRA.Function
 
 open LRA.Set
 universe u v
+
+/-- The declared source type of a relational function. -/
+abbrev DomainType {Domain : Type u} {Codomain : Type v}
+    (_function : RelationalFunction Domain Codomain) : Type u := Domain
+
+/-- The declared target type of a relational function. -/
+abbrev CodomainType {Domain : Type u} {Codomain : Type v}
+    (_function : RelationalFunction Domain Codomain) : Type v := Codomain
 
 /-- Domain class of a function, inherited from its graph relation. -/
 abbrev DomainClass {Domain : Type u} {Codomain : Type v}
@@ -35,6 +44,25 @@ abbrev PreimageClass {Domain : Type u} {Codomain : Type v}
 abbrev FiberClass {Domain : Type u} {Codomain : Type v}
     (function : RelationalFunction Domain Codomain) (output : Codomain) : SetClass Domain :=
   LRA.Relation.FiberClass function.graph output
+
+/-- Restrict the graph of a function to a source class. -/
+abbrev RestrictDomainGraph {Domain : Type u} {Codomain : Type v}
+    (function : RelationalFunction Domain Codomain) (source : SetClass Domain) :
+    FunctionRelation Domain Codomain :=
+  LRA.Relation.RestrictDomain function.graph source
+
+/-- Restrict the graph of a function to a target class. -/
+abbrev RestrictCodomainGraph {Domain : Type u} {Codomain : Type v}
+    (function : RelationalFunction Domain Codomain) (target : SetClass Codomain) :
+    FunctionRelation Domain Codomain :=
+  LRA.Relation.RestrictCodomain function.graph target
+
+/-- Restrict the graph of a function to source and target classes. -/
+abbrev RestrictGraph {Domain : Type u} {Codomain : Type v}
+    (function : RelationalFunction Domain Codomain)
+    (source : SetClass Domain) (target : SetClass Codomain) :
+    FunctionRelation Domain Codomain :=
+  LRA.Relation.Restrict function.graph source target
 
 /-- The value of a relational function at an input, selected from totality. -/
 noncomputable def Value {Domain : Type u} {Codomain : Type v}
