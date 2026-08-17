7a8291cdff89f9aa05b5f0af55c9a355ab0c2bc0
BoondockTaints <wsollers@gmail.com>
2026-08-17 17:35:39 -0400

    refactor: canonicalize preorder definition

---
+++ LRA/Order/OrderedSets/Preorder/Definition.lean
@@ -0,0 +1,25 @@
+import LRA.Order.Relation.OrderRelation.Definition
+import LRA.Relation.Properties.Definition
+
+namespace LRA.Order
+
+universe u
+
+/-- A preorder is a reflexive, transitive endorelation. -/
+def Preorder {Alpha : Type u}
+    (relation : LRA.Relation.Endorelation Alpha) : Prop :=
+  LRA.Relation.Reflexive relation ∧
+    LRA.Relation.Transitive relation
+
+end LRA.Order
+
+namespace LRA.Order.OrderedSets.Preorder
+
+universe u
+
+/-- A preorder relation bundled with its preorder law. -/
+structure PreorderRelation (Carrier : Type u) where
+  relation : LRA.Order.OrderedSets.OrderRelation Carrier
+  isPreorder : LRA.Order.Preorder relation
+
+end LRA.Order.OrderedSets.Preorder
