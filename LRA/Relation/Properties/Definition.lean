3ef7d0038f44fccac78e9ba66d30c7206cc027d8
BoondockTaints <wsollers@gmail.com>
2026-08-17 13:47:36 -0400

    refactor: canonicalize relation extremal-element properties

--- LRA/Relation/Properties/Definition.lean
+++ LRA/Relation/Properties/Definition.lean
@@ -1,7 +1,7 @@
 import LRA.Relation.Definition
 
 namespace LRA.Relation
-universe u
+universe u v
 
 /-- Reflexivity. -/
 def Reflexive {α : Type u} (R : Endorelation α) : Prop := ∀ x, R x x
@@ -36,5 +36,23 @@ def RightEuclidean {α : Type u} (R : Endorelation α) : Prop :=
 /-- Density of a relation. -/
 def Dense {α : Type u} (R : Endorelation α) : Prop :=
   ∀ x y, R x y → ∃ z, R x z ∧ R z y
+
+/-- A minimal element of a represented subset with respect to an endorelation. -/
+def MinimalElement {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    (relation : Endorelation Element)
+    (subset : SetObject)
+    (minimum : Element) : Prop :=
+  minimum ∈ subset ∧
+    ∀ element, element ∈ subset → ¬ relation element minimum
+
+/-- A maximal element of a represented subset with respect to an endorelation. -/
+def MaximalElement {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    (relation : Endorelation Element)
+    (subset : SetObject)
+    (maximum : Element) : Prop :=
+  maximum ∈ subset ∧
+    ∀ element, element ∈ subset → ¬ relation maximum element
 
 end LRA.Relation
