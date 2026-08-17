84cb538bb2b8ea611da064e067315fffeb451205
BoondockTaints <wsollers@gmail.com>
2026-08-17 10:37:11 -0400

    refactor: expand canonical Relation calculus

--- LRA/Relation/Calculus.lean
+++ LRA/Relation/Calculus.lean
@@ -1,4 +1,6 @@
 import LRA.Relation.Calculus.Classes.Definition
 import LRA.Relation.Calculus.Classes.Theorems
-import LRA.VolumeI.Relations.DomainRange.All
+import LRA.Relation.Calculus.DomainRange.Definition
+import LRA.Relation.Calculus.Restriction.Definition
+import LRA.VolumeI.Relations.DomainRange.Consequences
 import LRA.VolumeI.Relations.Images.All
