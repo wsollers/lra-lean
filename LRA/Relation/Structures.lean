d11ff787e770431fada70482194b151207de2a64
BoondockTaints <wsollers@gmail.com>
2026-08-17 15:29:00 -0400

    refactor: expose partial-function relation structure

--- LRA/Relation/Structures.lean
+++ LRA/Relation/Structures.lean
@@ -1,5 +1,6 @@
 import LRA.Relation.Structures.Definition
 import LRA.Relation.Structures.Relationships
 import LRA.Relation.Structures.Equivalence
+import LRA.Relation.Structures.PartialFunction
 
 /-! Canonical named relation structures. -/
