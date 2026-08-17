a8c87bf3e9acc62926bace08dbbdb3dcfd70d251
BoondockTaints <wsollers@gmail.com>
2026-08-17 17:49:54 -0400

    refactor: expose canonical foundational order theorem layers

--- LRA/Order/OrderedSets.lean
+++ LRA/Order/OrderedSets.lean
@@ -1,12 +1,19 @@
 import LRA.Order.OrderedSets.Preorder.Definition
 import LRA.Order.OrderedSets.Preorder.Characterizations
 import LRA.Order.OrderedSets.Preorder.Theorems
+import LRA.Order.OrderedSets.Preorder.Relationships
 import LRA.Order.OrderedSets.PartialOrder.Definition
 import LRA.Order.OrderedSets.PartialOrder.NonStrict
 import LRA.Order.OrderedSets.PartialOrder.Characterizations
+import LRA.Order.OrderedSets.PartialOrder.Relationships
 import LRA.Order.OrderedSets.LinearOrder.Definition
+import LRA.Order.OrderedSets.LinearOrder.Theorems
 import LRA.Order.OrderedSets.StrictOrder.Definition
+import LRA.Order.OrderedSets.StrictOrder.Characterizations
+import LRA.Order.OrderedSets.StrictOrder.Theorems
+import LRA.Order.OrderedSets.StrictOrder.Relationships
 import LRA.Order.OrderedSets.StrictLinearOrder.Definition
+import LRA.Order.OrderedSets.StrictLinearOrder.Theorems
 import LRA.Order.OrderedSets.Poset.Definition
 
 /-! Canonical foundational ordered-set hierarchy. -/
