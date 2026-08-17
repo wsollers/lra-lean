8e2123c457ca9fb60b7ab4aeee19811faac78886
BoondockTaints <wsollers@gmail.com>
2026-08-17 19:03:56 -0400

    refactor: canonicalize core Supremum theorems

---
+++ LRA/Order/Bounds/Supremum/Theorems.lean
@@ -0,0 +1,97 @@
+import LRA.Order.Bounds.Supremum.Definition
+import LRA.Relation.Properties.Definition
+
+namespace LRA.Order
+
+universe u v
+
+/-- Antisymmetry makes a supremum unique. -/
+theorem SupremumUnique
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    (relationIsAntisymmetric : LRA.Relation.Antisymmetric relation)
+    {subset : SetObject} {first second : Element}
+    (firstIsSupremum : Supremum relation subset first)
+    (secondIsSupremum : Supremum relation subset second) :
+    first = second := by
+  sorry
+
+/-- In a reflexive relation, an element is a supremum of its represented singleton. -/
+theorem SupremumOfSingleton
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject] [Singleton Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    (relationIsReflexive : LRA.Relation.Reflexive relation)
+    (element : Element)
+    (singletonMembership :
+      forall candidate,
+        candidate ∈ ({element} : SetObject) ↔ candidate = element) :
+    Supremum relation ({element} : SetObject) element := by
+  sorry
+
+/-- A supremum is an upper bound of the represented subset. -/
+theorem SupremumIsUpperBound
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    {subset : SetObject} {supremum : Element}
+    (supremumIsSupremum : Supremum relation subset supremum) :
+    UpperBound relation subset supremum :=
+  supremumIsSupremum.1
+
+/-- A supremum lies below every upper bound of the represented subset. -/
+theorem SupremumRelatedToEveryUpperBound
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    {subset : SetObject} {supremum bound : Element}
+    (supremumIsSupremum : Supremum relation subset supremum)
+    (boundIsUpperBound : UpperBound relation subset bound) :
+    relation supremum bound :=
+  supremumIsSupremum.2 bound boundIsUpperBound
+
+/-- Once a supremum exists, upper bounds are exactly the elements above it. -/
+theorem UpperBoundIffSupremumRelated
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    (relationIsTransitive : LRA.Relation.Transitive relation)
+    {subset : SetObject} {supremum bound : Element}
+    (supremumIsSupremum : Supremum relation subset supremum) :
+    UpperBound relation subset bound ↔ relation supremum bound := by
+  sorry
+
+/-- Suprema are monotone under inclusion of represented subsets. -/
+theorem SupremumMonotoneUnderInclusion
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    {smaller larger : SetObject} {smallerSupremum largerSupremum : Element}
+    (smallerIsContained : forall element, element ∈ smaller -> element ∈ larger)
+    (smallerSupremumIsSupremum :
+      Supremum relation smaller smallerSupremum)
+    (largerSupremumIsSupremum :
+      Supremum relation larger largerSupremum) :
+    relation smallerSupremum largerSupremum := by
+  sorry
+
+/-- Pointwise domination of one represented subset by another compares their suprema. -/
+theorem SupremaCompareUnderPointwiseDomination
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    (relationIsTransitive : LRA.Relation.Transitive relation)
+    {leftSubset rightSubset : SetObject}
+    {leftSupremum rightSupremum : Element}
+    (everyLeftHasLargerRight :
+      forall left, left ∈ leftSubset ->
+        exists right, right ∈ rightSubset /\ relation left right)
+    (leftSupremumIsSupremum :
+      Supremum relation leftSubset leftSupremum)
+    (rightSupremumIsSupremum :
+      Supremum relation rightSubset rightSupremum) :
+    relation leftSupremum rightSupremum := by
+  sorry
+
+end LRA.Order
