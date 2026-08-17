1934d0c4a4fdee85bb8b74c6afa31c8dcd65efae
BoondockTaints <wsollers@gmail.com>
2026-08-17 16:48:41 -0400

    refactor: canonicalize Absorbing law consequences

---
+++ LRA/Operation/Laws/Absorbing/Consequences.lean
@@ -0,0 +1,7 @@
+import LRA.Operation.Laws.Absorbing.Definition
+
+/-! Derived consequences of the Absorbing law belong here. -/
+
+namespace LRA.Operation.Laws.Absorbing
+
+end LRA.Operation.Laws.Absorbing
