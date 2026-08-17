df8ca58d983fbffe8c23dbefafa02b2fbe1f163a
BoondockTaints <wsollers@gmail.com>
2026-08-17 16:01:28 -0400

    refactor: order Volume I Relation before Function compatibility

--- LRA/VolumeI.lean
+++ LRA/VolumeI.lean
@@ -1,9 +1,9 @@
 import LRA.VolumeI.Logic
 import LRA.VolumeI.Identity
 import LRA.VolumeI.Set
+import LRA.VolumeI.Relations
 import LRA.VolumeI.Map
 import LRA.VolumeI.Operations
-import LRA.VolumeI.Relations
 import LRA.VolumeI.Order
 import LRA.VolumeI.UniversalAlgebra
 import LRA.VolumeI.AlgebraicStructures
