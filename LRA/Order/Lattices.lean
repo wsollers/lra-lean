e837f96b68964e6aee9d08d5dfdf249715035ab7
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:08:00 -0400

    refactor: add canonical lattices aggregate

---
+++ LRA/Order/Lattices.lean
@@ -0,0 +1,9 @@
+import LRA.Order.Lattices.Join.Definition
+import LRA.Order.Lattices.Meet.Definition
+import LRA.Order.Lattices.JoinSemilattice.Definition
+import LRA.Order.Lattices.MeetSemilattice.Definition
+import LRA.Order.Lattices.Lattice.Definition
+import LRA.Order.Lattices.DistributiveLattice.Definition
+import LRA.Order.Lattices.CompleteLattice.Definition
+
+/-! Canonical lattice vocabulary and structural hierarchy. -/
