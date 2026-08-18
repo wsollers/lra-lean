5520f2035375bdab033ce29328e034a62469ec42
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:51:36 -0400

    refactor: make legacy order isomorphism definition a shim

--- LRA/VolumeI/Order/Morphisms/OrderIsomorphism/Definition.lean
+++ LRA/VolumeI/Order/Morphisms/OrderIsomorphism/Definition.lean
@@ -1,20 +1,3 @@
-import LRA.VolumeI.Order.Morphisms.OrderEmbedding.Definition
+import LRA.Order.Morphisms.OrderIsomorphism.Definition
 
-namespace LRA.Order
-
-universe u v
-
-/--
-An order isomorphism consists of inverse maps whose forward map preserves and
-reflects order.
--/
-def OrderIsomorphism
-    {Alpha : Type u} {Beta : Type v}
-    (sourceRelation : LRA.Relation.Endorelation Alpha)
-    (targetRelation : LRA.Relation.Endorelation Beta)
-    (forward : Alpha -> Beta) (inverse : Beta -> Alpha) : Prop :=
-  (forall source, inverse (forward source) = source) /\
-    (forall target, forward (inverse target) = target) /\
-      OrderEmbedding sourceRelation targetRelation forward
-
-end LRA.Order
+/-! Legacy forwarding import. -/
