397b7ed3f73d97f22b3842aaa69e4b1581b5f1f1
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:43:48 -0400

    refactor: make legacy product order definition a shim

--- LRA/VolumeI/Order/Constructions/ProductOrder/Definition.lean
+++ LRA/VolumeI/Order/Constructions/ProductOrder/Definition.lean
@@ -1,16 +1,3 @@
-import LRA.VolumeI.Relations.Basic.Relations
+import LRA.Order.Constructions.ProductOrder.Definition
 
-namespace LRA.Order
-
-universe u v
-
-/-- The coordinatewise product of two relations. -/
-def ProductRelation
-    {Alpha : Type u} {Beta : Type v}
-    (leftRelation : LRA.Relation.Endorelation Alpha)
-    (rightRelation : LRA.Relation.Endorelation Beta) :
-    LRA.Relation.Endorelation (Alpha × Beta) :=
-  fun first second =>
-    leftRelation first.1 second.1 /\ rightRelation first.2 second.2
-
-end LRA.Order
+/-! Legacy forwarding import. -/
