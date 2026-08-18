de910c38489a282daffec4b4e9a32f1b2ae4a760
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:47:58 -0400

    refactor: canonicalize monotone order morphisms

---
+++ LRA/Order/Morphisms/Monotone/Definition.lean
@@ -0,0 +1,16 @@
+import LRA.Relation.Definition
+
+namespace LRA.Order
+
+universe u v
+
+/-- A map is monotone when it preserves the displayed relations. -/
+def Monotone
+    {Alpha : Type u} {Beta : Type v}
+    (sourceRelation : LRA.Relation.Endorelation Alpha)
+    (targetRelation : LRA.Relation.Endorelation Beta)
+    (map : Alpha -> Beta) : Prop :=
+  forall left right,
+    sourceRelation left right -> targetRelation (map left) (map right)
+
+end LRA.Order
