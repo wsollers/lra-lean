37105c9d15992587018008bf01db9845e3ccd38b
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:51:59 -0400

    refactor: split monotone core from legacy adapters

--- LRA/VolumeI/Order/Morphisms/Monotone/Theorems.lean
+++ LRA/VolumeI/Order/Morphisms/Monotone/Theorems.lean
@@ -1,4 +1,4 @@
-import LRA.VolumeI.Order.Morphisms.Monotone.Definition
+import LRA.Order.Morphisms.Monotone.Theorems
 import LRA.VolumeI.Map.Image.All
 import LRA.VolumeI.Map.Preimage.All
 import LRA.VolumeI.Order.Bounds.BoundSets.Theorems
@@ -8,25 +8,6 @@ namespace LRA.Order
 open LRA.Set
 
 universe u v w x
-
-/-- The identity map is monotone. -/
-theorem MonotoneIdentity
-    {Alpha : Type u}
-    (relation : LRA.Relation.Endorelation Alpha) :
-    Monotone relation relation (fun element => element) := by
-  sorry
-
-/-- Monotone maps compose. -/
-theorem MonotoneComp
-    {Alpha : Type u} {Beta : Type v} {Gamma : Type w}
-    {sourceRelation : LRA.Relation.Endorelation Alpha}
-    {middleRelation : LRA.Relation.Endorelation Beta}
-    {targetRelation : LRA.Relation.Endorelation Gamma}
-    {firstMap : Alpha -> Beta} {secondMap : Beta -> Gamma}
-    (firstMapIsMonotone : Monotone sourceRelation middleRelation firstMap)
-    (secondMapIsMonotone : Monotone middleRelation targetRelation secondMap) :
-    Monotone sourceRelation targetRelation (fun x => secondMap (firstMap x)) := by
-  sorry
 
 /-- `MonotoneSendsUpperBoundToImageUpperBound`
 
