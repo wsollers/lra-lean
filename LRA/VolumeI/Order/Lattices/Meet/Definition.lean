b5445b12bc026175e25d1d17a5904d9780f52deb
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:07:18 -0400

    refactor: make legacy meet definition a shim

--- LRA/VolumeI/Order/Lattices/Meet/Definition.lean
+++ LRA/VolumeI/Order/Lattices/Meet/Definition.lean
@@ -1,28 +1,3 @@
-import LRA.VolumeI.Order.OrderedSets.PartialOrder.Definition
+import LRA.Order.Lattices.Meet.Definition
 
-namespace LRA.Order
-
-universe u
-
-/--
-A meet of two elements is their greatest lower bound.
-
-Logical form:
-
-```lean
-def Meet {alpha : Type u}
-    (relation : LRA.Relation.Endorelation alpha)
-    (left right meet : alpha) : Prop :=
-  relation meet left /\ relation meet right /\
-    forall lower,
-      relation lower left -> relation lower right -> relation lower meet
-```
--/
-def Meet {alpha : Type u}
-    (relation : LRA.Relation.Endorelation alpha)
-    (left right meet : alpha) : Prop :=
-  relation meet left /\ relation meet right /\
-    forall lower,
-      relation lower left -> relation lower right -> relation lower meet
-
-end LRA.Order
+/-! Legacy forwarding import. -/
