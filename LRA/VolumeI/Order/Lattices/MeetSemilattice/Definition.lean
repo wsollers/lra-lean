2a56103e67842b08f98e5728e67aa58351019c97
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:07:32 -0400

    refactor: make legacy meet-semilattice definition a shim

--- LRA/VolumeI/Order/Lattices/MeetSemilattice/Definition.lean
+++ LRA/VolumeI/Order/Lattices/MeetSemilattice/Definition.lean
@@ -1,14 +1,3 @@
-import LRA.VolumeI.Order.Lattices.Meet.Definition
+import LRA.Order.Lattices.MeetSemilattice.Definition
 
-namespace LRA.Order
-
-universe u
-
-/-- A meet-semilattice is a partial order in which every pair has a meet. -/
-def MeetSemilattice
-    {Alpha : Type u}
-    (relation : LRA.Relation.Endorelation Alpha) : Prop :=
-  PartialOrder relation /\
-    forall left right, exists meet, Meet relation left right meet
-
-end LRA.Order
+/-! Legacy forwarding import. -/
