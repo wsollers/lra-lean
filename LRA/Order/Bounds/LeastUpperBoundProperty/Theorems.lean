71ac8057f4fa1b253c7b456ccf92d93f053f46f9
BoondockTaints <wsollers@gmail.com>
2026-08-17 21:06:54 -0400

    refactor: canonicalize least-upper-bound property theorem

---
+++ LRA/Order/Bounds/LeastUpperBoundProperty/Theorems.lean
@@ -0,0 +1,25 @@
+import LRA.Order.Bounds.LeastUpperBoundProperty.Definition
+import LRA.Order.Bounds.Supremum.Theorems
+
+namespace LRA.Order
+
+universe u v
+
+/-- The least-upper-bound property gives a unique supremum in an antisymmetric relation. -/
+theorem LeastUpperBoundPropertyGivesUniqueSupremum
+    {Element : Type u} {SetObject : Type v}
+    [Membership Element SetObject]
+    {relation : LRA.Relation.Endorelation Element}
+    (relationIsAntisymmetric : LRA.Relation.Antisymmetric relation)
+    (relationHasLeastUpperBounds :
+      LeastUpperBoundProperty SetObject relation)
+    (subset : SetObject)
+    (subsetIsNonempty : exists element, element ∈ subset)
+    (subsetIsBoundedAbove : BoundedAbove relation subset) :
+    exists supremum,
+      Supremum relation subset supremum /\
+        forall other,
+          Supremum relation subset other -> other = supremum := by
+  sorry
+
+end LRA.Order
