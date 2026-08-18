320afc02ed18c0e36faeb3c6797044c8f7769d1e
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:51:12 -0400

    refactor: make legacy monotone definition a shim

--- LRA/VolumeI/Order/Morphisms/Monotone/Definition.lean
+++ LRA/VolumeI/Order/Morphisms/Monotone/Definition.lean
@@ -1,16 +1,3 @@
-import LRA.VolumeI.Relations.Basic.Relations
+import LRA.Order.Morphisms.Monotone.Definition
 
-namespace LRA.Order
-
-universe u v
-
-/-- A map is monotone when it preserves the displayed relations. -/
-def Monotone
-    {Alpha : Type u} {Beta : Type v}
-    (sourceRelation : LRA.Relation.Endorelation Alpha)
-    (targetRelation : LRA.Relation.Endorelation Beta)
-    (map : Alpha -> Beta) : Prop :=
-  forall left right,
-    sourceRelation left right -> targetRelation (map left) (map right)
-
-end LRA.Order
+/-! Legacy forwarding import. -/
