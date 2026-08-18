8bd22eeb918b7ef695a0176f3f7789131a83069a
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:44:11 -0400

    refactor: make legacy lexicographic order theorems a shim

--- LRA/VolumeI/Order/Constructions/LexicographicOrder/Theorems.lean
+++ LRA/VolumeI/Order/Constructions/LexicographicOrder/Theorems.lean
@@ -1,18 +1,3 @@
-import LRA.VolumeI.Order.Constructions.LexicographicOrder.Definition
-import LRA.VolumeI.Order.OrderedSets.StrictLinearOrder.Definition
+import LRA.Order.Constructions.LexicographicOrder.Theorems
 
-namespace LRA.Order
-
-universe u v
-
-/-- The lexicographic product of strict linear orders is strict linear. -/
-theorem LexicographicProductIsStrictLinearOrder
-    {Alpha : Type u} {Beta : Type v}
-    {leftRelation : LRA.Relation.Endorelation Alpha}
-    {rightRelation : LRA.Relation.Endorelation Beta}
-    (leftIsStrictLinear : StrictLinearOrder leftRelation)
-    (rightIsStrictLinear : StrictLinearOrder rightRelation) :
-    StrictLinearOrder (LexicographicRelation leftRelation rightRelation) := by
-  sorry
-
-end LRA.Order
+/-! Legacy forwarding import. -/
