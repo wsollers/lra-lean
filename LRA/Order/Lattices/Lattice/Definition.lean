b71b26770966e11e4e18cb40d758dec90f8431db
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:06:47 -0400

    refactor: canonicalize lattice definition

---
+++ LRA/Order/Lattices/Lattice/Definition.lean
@@ -0,0 +1,16 @@
+import LRA.Order.Lattices.Join.Definition
+import LRA.Order.Lattices.Meet.Definition
+
+namespace LRA.Order
+
+universe u
+
+/-- Lattice laws: every pair has a join and a meet. -/
+def Lattice {alpha : Type u}
+    (relation : LRA.Relation.Endorelation alpha) : Prop :=
+  PartialOrder relation /\
+    forall left right : alpha,
+      (exists join, Join relation left right join) /\
+        (exists meet, Meet relation left right meet)
+
+end LRA.Order
