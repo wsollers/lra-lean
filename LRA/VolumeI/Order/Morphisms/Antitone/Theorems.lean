e1a280c879e0e49e6ac0ca5c725ed28f9dff6e25
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:52:21 -0400

    refactor: split antitone core from legacy adapters

--- LRA/VolumeI/Order/Morphisms/Antitone/Theorems.lean
+++ LRA/VolumeI/Order/Morphisms/Antitone/Theorems.lean
@@ -1,5 +1,4 @@
-import LRA.VolumeI.Order.Morphisms.Antitone.Definition
-import LRA.VolumeI.Order.Morphisms.Monotone.Definition
+import LRA.Order.Morphisms.Antitone.Theorems
 import LRA.VolumeI.Map.Image.All
 import LRA.VolumeI.Map.Preimage.All
 import LRA.VolumeI.Order.Bounds.BoundSets.Theorems
@@ -9,45 +8,6 @@ namespace LRA.Order
 open LRA.Set
 
 universe u v w x
-
-/-- Composing two antitone maps produces a monotone map. -/
-theorem AntitoneCompAntitoneIsMonotone
-    {Alpha : Type u} {Beta : Type v} {Gamma : Type w}
-    {sourceRelation : LRA.Relation.Endorelation Alpha}
-    {middleRelation : LRA.Relation.Endorelation Beta}
-    {targetRelation : LRA.Relation.Endorelation Gamma}
-    {firstMap : Alpha -> Beta} {secondMap : Beta -> Gamma}
-    (firstMapIsAntitone : Antitone sourceRelation middleRelation firstMap)
-    (secondMapIsAntitone : Antitone middleRelation targetRelation secondMap) :
-    Monotone sourceRelation targetRelation
-      (fun element => secondMap (firstMap element)) := by
-  sorry
-
-/-- An antitone map followed by a monotone map is antitone. -/
-theorem AntitoneThenMonotoneIsAntitone
-    {Alpha : Type u} {Beta : Type v} {Gamma : Type w}
-    {sourceRelation : LRA.Relation.Endorelation Alpha}
-    {middleRelation : LRA.Relation.Endorelation Beta}
-    {targetRelation : LRA.Relation.Endorelation Gamma}
-    {firstMap : Alpha -> Beta} {secondMap : Beta -> Gamma}
-    (firstMapIsAntitone : Antitone sourceRelation middleRelation firstMap)
-    (secondMapIsMonotone : Monotone middleRelation targetRelation secondMap) :
-    Antitone sourceRelation targetRelation
-      (fun element => secondMap (firstMap element)) := by
-  sorry
-
-/-- A monotone map followed by an antitone map is antitone. -/
-theorem MonotoneThenAntitoneIsAntitone
-    {Alpha : Type u} {Beta : Type v} {Gamma : Type w}
-    {sourceRelation : LRA.Relation.Endorelation Alpha}
-    {middleRelation : LRA.Relation.Endorelation Beta}
-    {targetRelation : LRA.Relation.Endorelation Gamma}
-    {firstMap : Alpha -> Beta} {secondMap : Beta -> Gamma}
-    (firstMapIsMonotone : Monotone sourceRelation middleRelation firstMap)
-    (secondMapIsAntitone : Antitone middleRelation targetRelation secondMap) :
-    Antitone sourceRelation targetRelation
-      (fun element => secondMap (firstMap element)) := by
-  sorry
 
 /-- `AntitoneSendsUpperBoundToImageLowerBound`
 
