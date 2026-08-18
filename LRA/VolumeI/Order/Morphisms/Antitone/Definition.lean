84e0bb6ca1d1537ba3c4d9e3ddc61bb84699b206
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:51:21 -0400

    refactor: make legacy antitone definition a shim

--- LRA/VolumeI/Order/Morphisms/Antitone/Definition.lean
+++ LRA/VolumeI/Order/Morphisms/Antitone/Definition.lean
@@ -1,16 +1,3 @@
-import LRA.VolumeI.Relations.Basic.Relations
+import LRA.Order.Morphisms.Antitone.Definition
 
-namespace LRA.Order
-
-universe u v
-
-/-- A map is antitone when it reverses the displayed relations. -/
-def Antitone
-    {Alpha : Type u} {Beta : Type v}
-    (sourceRelation : LRA.Relation.Endorelation Alpha)
-    (targetRelation : LRA.Relation.Endorelation Beta)
-    (map : Alpha -> Beta) : Prop :=
-  forall left right,
-    sourceRelation left right -> targetRelation (map right) (map left)
-
-end LRA.Order
+/-! Legacy forwarding import. -/
