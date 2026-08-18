bb662511a297bea093e2b777c6ef71b61d0d54dd
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:52:47 -0400

    refactor: split order embedding core from legacy adapters

--- LRA/VolumeI/Order/Morphisms/OrderEmbedding/Theorems.lean
+++ LRA/VolumeI/Order/Morphisms/OrderEmbedding/Theorems.lean
@@ -1,44 +1,15 @@
-import LRA.VolumeI.Order.Morphisms.OrderEmbedding.Definition
-import LRA.VolumeI.Order.OrderedSets.PartialOrder.Definition
+import LRA.Order.Morphisms.OrderEmbedding.Theorems
 import LRA.VolumeI.Map.Image.All
 import LRA.VolumeI.Map.Preimage.All
 import LRA.VolumeI.Order.Bounds.BoundSets.Theorems
-import LRA.VolumeI.Order.Bounds.GreatestElement.Definition
-import LRA.VolumeI.Order.Bounds.LeastElement.Definition
+import LRA.Order.Bounds.GreatestElement.Definition
+import LRA.Order.Bounds.LeastElement.Definition
 
 namespace LRA.Order
 
 open LRA.Set
 
 universe u v w x
-
-/-- An embedding between partial orders is injective. -/
-theorem OrderEmbeddingInjective
-    {Alpha : Type u} {Beta : Type v}
-    {sourceRelation : LRA.Relation.Endorelation Alpha}
-    {targetRelation : LRA.Relation.Endorelation Beta}
-    {map : Alpha -> Beta}
-    (sourceIsAntisymmetric :
-      LRA.Relation.Antisymmetric sourceRelation)
-    (targetIsReflexive : LRA.Relation.Reflexive targetRelation)
-    (mapIsEmbedding : OrderEmbedding sourceRelation targetRelation map) :
-    Function.Injective map := by
-  sorry
-
-/-- Order embeddings compose. -/
-theorem OrderEmbeddingComp
-    {Alpha : Type u} {Beta : Type v} {Gamma : Type w}
-    {sourceRelation : LRA.Relation.Endorelation Alpha}
-    {middleRelation : LRA.Relation.Endorelation Beta}
-    {targetRelation : LRA.Relation.Endorelation Gamma}
-    {firstMap : Alpha -> Beta} {secondMap : Beta -> Gamma}
-    (firstMapIsEmbedding :
-      OrderEmbedding sourceRelation middleRelation firstMap)
-    (secondMapIsEmbedding :
-      OrderEmbedding middleRelation targetRelation secondMap) :
-    OrderEmbedding sourceRelation targetRelation
-      (fun element => secondMap (firstMap element)) := by
-  sorry
 
 /-- `OrderEmbeddingSendsUpperBoundToImageUpperBound`
 
