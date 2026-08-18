c4bded4666f54af717cc49431a525b65aeeccb41
BoondockTaints <wsollers@gmail.com>
2026-08-17 20:43:55 -0400

    refactor: make legacy product order theorems a shim

--- LRA/VolumeI/Order/Constructions/ProductOrder/Theorems.lean
+++ LRA/VolumeI/Order/Constructions/ProductOrder/Theorems.lean
@@ -1,18 +1,3 @@
-import LRA.VolumeI.Order.Constructions.ProductOrder.Definition
-import LRA.VolumeI.Order.OrderedSets.PartialOrder.Definition
+import LRA.Order.Constructions.ProductOrder.Theorems
 
-namespace LRA.Order
-
-universe u v
-
-/-- The product of two partial orders is a partial order. -/
-theorem ProductOfPartialOrdersIsPartialOrder
-    {Alpha : Type u} {Beta : Type v}
-    {leftRelation : LRA.Relation.Endorelation Alpha}
-    {rightRelation : LRA.Relation.Endorelation Beta}
-    (leftIsPartialOrder : PartialOrder leftRelation)
-    (rightIsPartialOrder : PartialOrder rightRelation) :
-    PartialOrder (ProductRelation leftRelation rightRelation) := by
-  sorry
-
-end LRA.Order
+/-! Legacy forwarding import. -/
