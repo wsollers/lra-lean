74e6e814d3b1346315975d3fa9e0da496efe3b76
BoondockTaints <wsollers@gmail.com>
2026-08-17 21:05:07 -0400

    refactor: add opt-in Mathlib order interoperability aggregate

---
+++ LRA/Order/Interop/Mathlib.lean
@@ -0,0 +1,5 @@
+import LRA.Order.Interop.Mathlib.Certificates
+import LRA.Order.Interop.Mathlib.Bounds
+import LRA.Order.Interop.Mathlib.LinearOrder
+
+/-! Opt-in Mathlib interoperability for canonical Order. Not imported by `LRA.Order`. -/
