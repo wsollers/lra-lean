ec90c5139fac2d48ed72da07fc3e6d51880298a4
William E. Sollers III <wsollers@gmail.com>
2026-08-21 14:05:48 -0400

    Standardize OrderedGroup model theory layout

--- LRA/AlgebraicStructures/OrderedGroup.lean
+++ LRA/AlgebraicStructures/OrderedGroup.lean
@@ -1,8 +1,8 @@
 import LRA.AlgebraicStructures.OrderedGroup.Definition
 import LRA.AlgebraicStructures.OrderedGroup.Interface.Signature.Definition
+import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.LStructure
+import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.Theory
 import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.Model
-import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.Axioms
-import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.Satisfaction
 import LRA.AlgebraicStructures.OrderedGroup.Theorems
 import LRA.AlgebraicStructures.OrderedGroup.Characterizations
 import LRA.AlgebraicStructures.OrderedGroup.Consequences
--- LRA/AlgebraicStructures/OrderedGroup/Relationships.lean
+++ LRA/AlgebraicStructures/OrderedGroup/Relationships.lean
@@ -1,5 +1,5 @@
 import LRA.AlgebraicStructures.OrderedGroup.Laws.Definition
-import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.Satisfaction
+import LRA.AlgebraicStructures.OrderedGroup.Interface.ModelTheory.Model
 
 namespace LRA.AlgebraicStructures
 
